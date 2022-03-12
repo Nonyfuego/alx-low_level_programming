94254890d164dd500c5735a1efb681e36899e29c
Nonyfuego <Nonykuti@gmail.com>
2022-03-12 20:49:57 +0100

    delete size 32 , size 64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int a;
@@ -19,6 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-
 	return (0);
 }
