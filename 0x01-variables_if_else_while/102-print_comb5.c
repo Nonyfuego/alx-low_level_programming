83d19f7320ac4e9478269cdf26889da7bd49a962
Nonyfuego <Nonykuti@gmail.com>
2022-03-15 04:40:07 +0100

    add 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,23 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 98; a++)
+	for (a = 0; a <= 99; a++)
 	{
-		for (b = a + 1; b <= 99; b++)
+		for (b = 0; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b / 10) + '0');
-			if (a == 98 && b == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (a < b && a != b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
