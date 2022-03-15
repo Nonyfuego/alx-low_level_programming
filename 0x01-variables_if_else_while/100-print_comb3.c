012c63bf71d65117d0533deb8af854d39cb5c262
Nonyfuego <Nonykuti@gmail.com>
2022-03-15 02:50:14 +0100

    add print combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,20 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; b <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-
-			if (a < 8)
+			if (a < b)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(a);
+				putchar(b);
+
+				if (a != '8' || (a  == '8' && b != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
