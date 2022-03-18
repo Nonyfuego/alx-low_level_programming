6c2d91946887985f69b6e4545dbdefd0abb10bab
Nonyfuego <Nonykuti@gmail.com>
2022-03-19 00:43:02 +0100

    add lower case program

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,15 +11,15 @@
 int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int b);
-int _isalpha(int b);
-int print_sign(int b);
-int _abs(int b);
-int print_last_digit(int b);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int c);
+int print_last_digit(int c);
 void jack_bauer(void);
 void times_table(void);
 int add (int a, int b);
-void print_to_98(int b);
-void print_times_table(int b);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
