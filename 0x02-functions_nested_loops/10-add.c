5d2a13d6e81752c172e29bd545ac15c1de0f64a0
Nonyfuego <Nonykuti@gmail.com>
2022-03-20 14:06:07 +0100

    added the add fuctions and times atbel function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function that add two integers and returns the result.
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
